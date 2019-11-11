965ca7a42ee1969859c013bab78f019c8d8def23
Syed Afzal <sah.afzal@gmail.com>
2019-11-11 18:16:20 +0500

    test cases for action generators

--- src/actions/index.test.js
+++ src/actions/index.test.js
@@ -12,7 +12,7 @@ describe('actions', () => {
         store = mockStore();
     });
 
-    it('should create an action to toogle a todo', () => {
+    it('should create an action to TOGGLE a TODO', () => {
         const id = '112121csdfsdf-dsf2df456sdf';
         const expectedAction = {
             type: "TOGGLE_TODO",
@@ -20,5 +20,45 @@ describe('actions', () => {
         };
         store.dispatch(actions.toggleTodo('112121csdfsdf-dsf2df456sdf'));
         expect(store.getActions()[0]).toEqual(expectedAction);
-    })
+    });
+
+    it('should create an action to DELETE a TODO', () => {
+        const id = '112121csdfsdf-dsf2df456sdf';
+        const expectedAction = {
+            type: "DELETE_TODO",
+            id
+        };
+        store.dispatch(actions.deleteTodo('112121csdfsdf-dsf2df456sdf'));
+        expect(store.getActions()[0]).toEqual(expectedAction);
+    });
+
+    it('should create an action to RESTORE a TODO', () => {
+        const id = '112121csdfsdf-dsf2df456sdf';
+        const expectedAction = {
+            type: "RESTORE_TODO",
+            id
+        };
+        store.dispatch(actions.restoreTodo('112121csdfsdf-dsf2df456sdf'));
+        expect(store.getActions()[0]).toEqual(expectedAction);
+    });
+
+    it('should create an action to EDIT a TODO', () => {
+        const id = '112121csdfsdf-dsf2df456sdf';
+        const expectedAction = {
+            type: "EDIT_TODO",
+            id
+        };
+        store.dispatch(actions.editTodo('112121csdfsdf-dsf2df456sdf'));
+        expect(store.getActions()[0]).toEqual(expectedAction);
+    });
+
+    it('should change NAVIGATION', () => {
+        const index = 0;
+        const expectedAction = {
+            type: "CHANGE_CURRENT_NAV",
+            index
+        };
+        store.dispatch(actions.changeIndex(index));
+        expect(store.getActions()[0]).toEqual(expectedAction);
+    });
 });
